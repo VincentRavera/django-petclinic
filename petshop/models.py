3a7e7f8ba21d44c912221c05604d5c3548b487ed
Vincent <ravera.vincent@gmail.com>
2020-03-01 18:17:48 +0100

    Init README and models

--- petshop/models.py
+++ petshop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+
+class Sex(Enum):
+    UNKNOWN = 0
+    FEMALE = 1
+    MALE = 2
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+class Owner(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Animal(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    sex = models.IntegerField(choices=Sex.choices(), default=Sex.UNKNOWN)
+
+    def __str__(self):
+        return self.name
+
+class RendezVous(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    date = models.DateTimeField('Appointement')
--- petshop/tests.py
+++ petshop/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from .models import Owner, Animal, RendezVous
 
 # Create your tests here.
+
+# Utils Functions
+
+owners_created = []
+def create_owner(name, number):
+    """
+    Create an Owner with the given `name` and his phone `number`.
+    """
+    owner = Owner.objects.create(name=name, number=number)
+    owners_created.append(owner)
+    return owner
+animals_created = []
+def create_animal(name, number):
+    """
+    Create an Animal with the given `name` and his phone `number`.
+    """
+    animal = Animal.objects.create(name=name, number=number)
+    animals_created.append(animal)
+    return animal
+rendezVouss_created = []
+def create_rendezVous(name, number):
+    """
+    Create an RendezVous with the given `name` and his phone `number`.
+    """
+    rendezVous = RendezVous.objects.create(name=name, number=number)
+    rendezVouss_created.append(rendezVous)
+    return rendezVous
+
+def clean_owners():
+    for owner in owners_created:
+        owner.delete()
+
+def clean_animals():
+    for animal in animals_created:
+        animal.delete()
+
+def clean_rendezVouss():
+    for rendezVous in rendezVouss_created:
+        rendezVous.delete()
+
+def clean_data():
+    clean_owners()
+    clean_animals()
+    clean_rendezVouss()
+# Views Tests
+
+# Models test
+class OwnerModelTests(TestCase):
+    pass
+class AnimalModelTests(TestCase):
+    pass
+class RendezVousModelTests(TestCase):
+    pass
