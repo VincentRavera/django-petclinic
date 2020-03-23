1f4f0a7398a0b7638ac7e1d046d2dcb7d076f193
Vincent <ravera.vincent@gmail.com>
2020-03-23 14:16:26 +0100

    Simple view for a bare-boned petshop site

--- petshop/urls.py
+++ petshop/urls.py
@@ -6,13 +6,17 @@ from . import views
 app_name="petshop"
 urlpatterns = [
     # /petshop/owner
+    path('', views.IndexView.as_view(), name='index'),
     # List owners
-    path('owner', views.IndexView.as_view(), name='index'),
+    path('owner', views.owner_list, name='owners'),
 
     # /petshop/owner/5
     path('owner/<int:pk>', views.owner_view, name='owner'),
 
-    # /petshop/owner/5/pet/1
-    #path('/owner/<int:pk>/pet/<int:pk>', views.AnimalView.as_view(), name='animal'),
+    # /petshop/animal
+    path('animal', views.animal_list, name='animals'),
+
+    # /petshop/animal/7
+    path('animal/<int:pk>', views.animal_view, name='animal'),
 
 ]
--- petshop/views.py
+++ petshop/views.py
@@ -2,18 +2,33 @@ from django.shortcuts import render, get_object_or_404, HttpResponse
 from django.urls import reverse
 from django.views import generic
 
-from .models import Owner
+from .models import Owner, Animal
 
 # Create your views here.
 
 class IndexView(generic.ListView):
-    template_name = 'petshop/index.html.j2'
+    template_name = 'petshop/index.html'
     context_object_name = 'owner_list'
 
     def get_queryset(self):
+        """Returns all Owner"""
         return Owner.objects.all()
+
+
+def owner_list(request):
+    owners = Owner.objects.all()
+    return render(request, 'petshop/owner_list.html', {'owner_list': owners})
+
 
 def owner_view(request, pk):
     owner = get_object_or_404(Owner, pk=pk)
     animals = owner.animal_set.all()
-    return render(request, 'petshop/owner_view.html.j2', {'owner': owner, 'animals_list': animals})
+    return render(request, 'petshop/owner_view.html', {'owner': owner, 'animals_list': animals})
+
+def animal_view(request, pk):
+    animal = get_object_or_404(Animal, pk=pk)
+    return render(request, 'petshop/animal_view.html', {'animal': animal})
+
+def animal_list(request):
+    animals = Animal.objects.all()
+    return render(request, 'petshop/animal_list.html', {'animal_list': animals})
